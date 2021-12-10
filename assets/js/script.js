ea835a8f40536e1c4f74b846b0d5838d12bc4ad6
Gerardo Soto Contreras <gerardo.asc@icloud.com>
2021-12-09 23:01:11 -0300

    segundo commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+window.alert("Bienvenido a Viajes Chile");
+
+$('#quienessomos').click(function () {
+    alert("")
+});
+
+$('#destacados').click(function () {
+    alert("")
+});
+
+$('#contacto').click(function () {
+    alert("")
+});
